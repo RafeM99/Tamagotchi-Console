ec5ca44abe70da90b6436d1aa43c99c30127d1b9
Rafe Marriott <Marriott.R@etoncollege.org.uk>
2024-11-22 17:10:43 +0000

    going well

--- Tamagotchi Console/main.swift
+++ Tamagotchi Console/main.swift
@@ -9,8 +9,8 @@ import Foundation
 
 class Tamagotchi {
     let name: String
-    let age: Int
-    let weight: Double
+    var age: Int
+    var weight: Double
     let colour: String
     var hunger: Int
     var health: Int
@@ -36,6 +36,43 @@ class Tamagotchi {
         print()
     }
     
+    func displayMenu() {
+        print("""
+
+----------------
+1. Feed
+2. Give Medicine
+3. Work
+4. Rest
+
+5. Quit
+----------------
+Enter choice:
+""")
+    }
+    
+    func randomEvent() {
+        let result = Int.random(in: 1...4)
+        
+        if result == 1 {
+            print("\(name) has got sick!")
+            let healthDeduction = Int.random(in: 5...100)
+            print("-\(healthDeduction) health")
+            health -= healthDeduction
+        } else if result == 2 {
+            print("\(name) has grown up!")
+            print("+1 age")
+            age += 1
+        } else if result == 3 {
+            print("\(name) did some exercise!")
+            print("+5 health, -5 energy")
+            health += 5
+            energy -= 5
+        } else if result == 4 {
+            print("\(name) did nothing!")
+        }
+    }
+    
     func feed() {
         hunger += 5
     }
@@ -43,6 +80,15 @@ class Tamagotchi {
     func giveMedicine() {
         health += 10
     }
+    
+    func work() {
+        energy -= 10
+    }
+    
+    func rest() {
+        
+    }
+    
 }
 
 print("Enter your Tamagotchi's name: ")
@@ -53,13 +99,40 @@ let colour = readLine() ?? "red"
 
 let t = Tamagotchi(name, colour)
 
+print("\(t.name) was born!")
+print("They are \(t.colour)!")
+
 var quit = false
 
 while quit == false {
-    print("\(t.name) was born!")
-    print("They are \(t.colour)!")
+    print()
+    t.randomEvent()
+    
     t.showStats()
+    t.displayMenu()
     
+    let input = readLine() ?? ""
     
+    if input == "1" {
+        print("Feeding \(t.name)")
+        t.feed()
+    } else if input == "2" {
+        print("Giving medicine to \(t.name)")
+        t.giveMedicine()
+    } else if input == "3" {
+        print("Putting \(t.name) to work!")
+        t.work()
+    } else if input == "4" {
+        print("Letting \(t.name) rest")
+        t.rest()
+    } else if input == "5" {
+        print("Quitting...")
+        quit = true
+    }
+    
+    if t.health <= 0 {
+        print("\(t.name) died!")
+        quit = true
+    }
 }
 
